ed5ef5d075241458568761896da42cd884d84d44
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-11-23 18:30:56 +0000

    feat(Consumer): creation of the consumer constructor and executable method of the thread so that a vegetable was added to the warehouse

--- src/com/stardewvalley/threads/Consumer.java
+++ src/com/stardewvalley/threads/Consumer.java
@@ -1,5 +1,23 @@
 package com.stardewvalley.threads;
 
-public class Consumer {
-  
+import java.util.Scanner;
+
+import com.stardewvalley.manager.WareHouse;
+
+public class Consumer extends Thread{
+  private static Scanner scanner = new Scanner(System.in);
+  private WareHouse wareHouse;
+
+  public Consumer(WareHouse wareHouse) {
+    this.wareHouse = wareHouse;
+  }
+
+  @Override
+  public void run() {
+    System.out.println("Cuantas verduras quieres comprar?");
+        System.out.println("Ingrese nombre de la verdura a comprar:");
+        String vegetable = scanner.nextLine();
+        this.wareHouse.addVegetable(vegetable);
+    super.run();
+  }
 }
