a61ac8f46b57cf98592e73f831ff9a4d2fa86838
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-11-23 22:53:38 +0000

    fix(Consumer): The logic of the class has been changed, adding methods and modifying the run method

--- src/com/stardewvalley/threads/Consumer.java
+++ src/com/stardewvalley/threads/Consumer.java
@@ -5,19 +5,38 @@ import java.util.Scanner;
 import com.stardewvalley.elements.Restaurant;
 
 public class Consumer extends Thread {
-  private static Scanner scanner = new Scanner(System.in);
-  private Restaurant wareHouse;
 
-  public Consumer(Restaurant wareHouse) {
+  private String consumerName = "";
+  private int vegetableQuantity;
+  private Restaurant wareHouse;
+  private final int MAX_GROWTH_TIME = 2000; 
+
+
+  public Consumer(String consumerName, int vegetableQuantity, Restaurant wareHouse) {
+    this.consumerName = consumerName;
+    this.vegetableQuantity = vegetableQuantity;
     this.wareHouse = wareHouse;
+  }
+
+  private int consumeTime() {
+    int consumeTime = (int) Math.floor(Math.random() * MAX_GROWTH_TIME) + 1000;
+    return consumeTime > MAX_GROWTH_TIME ? MAX_GROWTH_TIME : consumeTime;
+  }
+
+  public String getConsumerName() {
+    return consumerName;
   }
 
   @Override
   public void run() {
-    System.out.println("Cuantas verduras quieres comprar?");
-    System.out.println("Ingrese nombre de la verdura a comprar:");
-    String vegetable = scanner.nextLine();
-    this.wareHouse.removeVegetable(vegetable);
-    super.run();
+    try {
+      for (int i = 0; i < vegetableQuantity; i++) {
+        Thread.sleep(consumeTime());
+        this.wareHouse.removeVegetable(getConsumerName());
+        super.run();
+      }
+    } catch (Exception e) {
+      System.out.println("Se ha producido un error en el consumo de los vegetales! ");
+    }
   }
 }
