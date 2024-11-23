99dc9e723dd400bcd90abd05c8c29263184cb056
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-11-23 18:33:04 +0000

    feat(Producer): creation of the producer constructor and executable method of the thread so that a vegetable was added to the warehouse

--- src/com/stardewvalley/threads/Producer.java
+++ src/com/stardewvalley/threads/Producer.java
@@ -1,5 +1,30 @@
 package com.stardewvalley.threads;
 
-public class Producer {
-  
+import java.util.Scanner;
+
+public class Producer extends Thread{
+  private static Scanner scanner = new Scanner(System.in);
+  private WareHouse wareHouse;
+  private int timeGrowth = 2000;
+
+  public Producer(WareHouse wareHouse) {
+    this.wareHouse = wareHouse;
+  }
+
+  @Override
+  public void run() {
+    System.out.println("Cuantas verduras quieres sembrar?");
+    int numVegetables = scanner.nextInt();
+    try {
+      for (int i = 0; i < numVegetables; i++) {
+        System.out.println("Ingrese nombre de la verdura a sembrar:");
+        String vegetable = scanner.nextLine();
+        Thread.sleep(timeGrowth);
+        this.wareHouse.removeVegetable(vegetable);
+      }
+    } catch (InterruptedException e) {
+      System.out.println("No se respeto el tiempo de crecimiento, intente de nuevo");
+    }
+    super.run();
+  }
 }
