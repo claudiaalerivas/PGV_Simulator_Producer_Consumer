b2f1a152061da2cad761a2beec947586a07dc10a
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2024-11-24 01:05:15 +0100

    feat(App): Added main program

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
+import com.stardewvalley.elements.Restaurant;
+import com.stardewvalley.threads.Consumer;
+import com.stardewvalley.threads.Producer;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) throws Exception {
+    Restaurant restaurant = new Restaurant("La huerta");
+
+    System.out.println("----" + restaurant.getName().toUpperCase() + "----");
+
+    Producer producer = new Producer("Paco", 10, restaurant);
+    Producer producer2 = new Producer("Ramon", 10, restaurant);
+    Producer producer3 = new Producer("APOWJAIODJWA", 10, restaurant);
+    Producer producer4 = new Producer("we", 10, restaurant);
+    Consumer consumer = new Consumer("Faustino", 10, restaurant);
+    Consumer consumer2 = new Consumer("Mr.Gentleman", 10, restaurant);
+    Consumer consumer3 = new Consumer("Loquendo", 20, restaurant);
+    producer.start();
+    producer2.start();
+    producer3.start();
+    producer4.start();
+    consumer.start();
+    consumer2.start();
+    consumer3.start();
+    producer.join();
+    producer2.join();
+    consumer.join();
+    consumer2.join();
+    consumer3.join();
+    System.out.println("El restaurante ha sido cerrado");
+  }
 }
