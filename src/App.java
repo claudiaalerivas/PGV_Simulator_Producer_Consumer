2edc98daa8abeaa8c8d823d6cc0c139040b5d8e3
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-11-25 16:32:01 +0000

    feat(App): experiment one performed

--- src/App.java
+++ src/App.java
@@ -8,25 +8,13 @@ public class App {
 
     System.out.println("----" + restaurant.getName().toUpperCase() + "----");
 
-    Producer producer = new Producer("Paco(Granjero)", 10, restaurant);
-    Producer producer2 = new Producer("Ramon(Granjero)", 10, restaurant);
-    Producer producer3 = new Producer("Kevin(Cliente)", 10, restaurant);
-    Producer producer4 = new Producer("Xiao(Cliente)", 10, restaurant);
-    Consumer consumer = new Consumer("Faustino(Cliente)", 10, restaurant);
-    Consumer consumer2 = new Consumer("Mr.Gentleman(Cliente)", 10, restaurant);
-    Consumer consumer3 = new Consumer("Loquendo(Cliente)", 20, restaurant);
+    Producer producer = new Producer("Paco(Granjero)", 5, restaurant);
+    Consumer consumer = new Consumer("Faustino(Cliente)", 5, restaurant);
+    
     producer.start();
-    producer2.start();
-    producer3.start();
-    producer4.start();
     consumer.start();
-    consumer2.start();
-    consumer3.start();
     producer.join();
-    producer2.join();
     consumer.join();
-    consumer2.join();
-    consumer3.join();
     System.out.println("El restaurante ha sido cerrado");
   }
 }
